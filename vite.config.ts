97b4fec8d982ae7cff2f12c09843b9ddb0483fc7
Rafael Orbe <rafaelorbe@outlook.com>
2026-03-28 12:20:07 -0300

    feat: agregar configuración de chunks manuales en la construcción de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,28 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+                return 'vendor-react';
+              }
+
+              if (id.includes('swiper')) {
+                return 'vendor-swiper';
+              }
+
+              if (id.includes('flowbite')) {
+                return 'vendor-flowbite';
+              }
+
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
   };
 });
