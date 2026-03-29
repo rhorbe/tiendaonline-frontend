c6f2d374238d3c5adaab5fbf29970f2dd24caecb
Rafael Orbe <rafaelorbe@outlook.com>
2026-03-29 12:37:54 -0300

    wip

--- src/core/api/axiosInstance.ts
+++ src/core/api/axiosInstance.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
-import { API_URL } from '../config/env';
+// import { API_URL } from '../config/env';
 import { setupInterceptors } from './interceptors';
 
 const api: AxiosInstance = axios.create({
-  baseURL: API_URL,
+  // baseURL: API_URL,
+  baseURL: 'https://tiendaonline-backend-git-dev-rhorbes-projects.vercel.app/backend',
   headers: { 'Content-Type': 'application/json' },
 });
 
