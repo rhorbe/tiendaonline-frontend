3a7733fe309e4942aeed6b59a47622c0b606850b
Rafael Orbe <rafaelorbe@outlook.com>
2026-04-16 12:58:01 -0300

    fix: Update redux-persist storage import and upgrade dependencies

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,7 +11,7 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/es/storage";
 
 import { UserReducer } from "./features/user.slice";
 
