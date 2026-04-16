f091e7560bfc0aa7318f817167007c5de166f538
Rafael Orbe <rafaelorbe@outlook.com>
2026-04-16 13:45:21 -0300

    chore: Remove unused CSS classes and flowbite plugin from configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -34,7 +33,5 @@ module.exports = {
       }
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 };
