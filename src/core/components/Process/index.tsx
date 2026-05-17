6460c47bc08f4146d9bd6352800016097ebd9aee
Rafael Orbe <rafaelorbe@outlook.com>
2026-05-17 17:49:48 -0300

    feat(process): add routing support for steps in Process component

--- src/core/components/Process/index.tsx
+++ src/core/components/Process/index.tsx
@@ -1,66 +1,70 @@
-import { FC } from "react";
+import {FC} from "react";
+import {Link} from "react-router-dom";
+import {ROUTES} from "@/core/enum/common.ts";
 
 interface ProcessProps {
-  activeStep: number;
-  completedStep?: number;
+    activeStep: number;
+    completedStep?: number;
 }
 
-const Process: FC<ProcessProps> = ({ activeStep, completedStep }) => {
-  const steps = [
-    { id: 1, label: "Carrito de compras" },
-    { id: 2, label: "Detalles del pago" },
-    { id: 3, label: "Completar pedido" },
-  ];
+const Process: FC<ProcessProps> = ({activeStep, completedStep}) => {
+    const steps = [
+        {id: 1, label: "Carrito de compras", link: ROUTES.CART},
+        {id: 2, label: "Detalles del pago", link:"#"},
+        {id: 3, label: "Completar pedido" , link:"#"},
+    ];
 
-  return (
-    <div className="flex gap-8 items-start w-fit md:mx-auto">
-      {steps.map((step) => {
-        const isActive = activeStep === step.id;
-        const isCompleted = completedStep && step.id <= completedStep;
+    return (
+        <div className="flex gap-8 items-start w-fit md:mx-auto">
+            {steps.map((step) => {
+                const isActive = activeStep === step.id;
+                const isCompleted = completedStep && step.id <= completedStep;
 
-        return (
-          <div
-            key={step.id}
-            className={`pb-6 flex items-center gap-4 
+                return (
+                    <div
+                        key={step.id}
+                        className={`pb-6 flex items-center gap-4 
                             ${
-                              isActive
+                            isActive
                                 ? "border-b-2 border-app-black"
                                 : isCompleted
-                                ? "border-b-2 border-app-green"
-                                : ""
-                            } 
+                                    ? "border-b-2 border-app-green"
+                                    : ""
+                        } 
                             ${isActive ? "block" : "hidden"} 
                             md:flex`}
-          >
-            <p
-              className={`py-2 px-[17px] rounded-[40px] text-white text-center font-inter text-base/[26px] font-semibold 
+                    >
+                        <p
+                            className={`py-2 px-[17px] rounded-[40px] text-white text-center font-inter text-base/[26px] font-semibold 
                             ${
-                              isActive
-                                ? "bg-app-black"
-                                : isCompleted
-                                ? "bg-app-green"
-                                : "bg-muted-gray"
+                                isActive
+                                    ? "bg-app-black"
+                                    : isCompleted
+                                        ? "bg-app-green"
+                                        : "bg-muted-gray"
                             }`}
-            >
-              {step.id}
-            </p>
-            <p
-              className={`font-inter text-base/[26px] font-semibold 
+                        >
+                            {step.id}
+                        </p>
+                        <p
+                            className={`font-inter text-base/[26px] font-semibold 
                             ${
-                              isActive
-                                ? "text-app-black"
-                                : isCompleted
-                                ? "text-app-green"
-                                : "text-muted-gray"
+                                isActive
+                                    ? "text-app-black"
+                                    : isCompleted
+                                        ? "text-app-green"
+                                        : "text-muted-gray"
                             }`}
-            >
-              {step.label}
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+                        >
+                            <Link to={step.link}>
+                                {step.label}
+                            </Link>
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Process;
