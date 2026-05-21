5c441d0c525f01001544ab322fa590b54e4f9bbe
Rafael Orbe <rafaelorbe@outlook.com>
2026-05-21 08:24:00 -0300

    wip

--- src/core/api/pedidosApi.ts
+++ src/core/api/pedidosApi.ts
@@ -2,8 +2,13 @@ import api from "./axiosInstance";
 import type { Pedido, PedidosResponse } from "../models/Pedido";
 
 export const fetchPedidos = async (clienteId: string): Promise<PedidosResponse> => {
+
+  console.info(`Obteniendo pedidos para cliente ID: ${clienteId}`);
+
   const response = await api.get<PedidosResponse>(`/cliente/${clienteId}/pedidos`);
   const data = response.data;
+
+  console.info(response.data);
 
   if (!Array.isArray(data)) {
     throw new Error("No se pudieron obtener los pedidos");
